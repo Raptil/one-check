7371d6ba9d8dae99d033e88694a2b1cf90054fd9
Raptil <Hurricane095@gmail.com>
2020-03-21 00:20:15 +0300

    Initial commit

---
+++ src/main/java/org/startup/onecheck/OneCheckApplication.java
@@ -0,0 +1,13 @@
+package org.startup.onecheck;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCheckApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(OneCheckApplication.class, args);
+        }
+}
