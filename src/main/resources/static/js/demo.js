bcfeca7c4fb3472d9165449aabe95c6908f3acb6
Pushkin Sergey <pushkinsergey.voronezh@gmail.com>
2020-03-21 17:29:49 +0300

    Add draw products

--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -1,3 +1,34 @@
-window.onload = function(){
-        alert('Страница загружена');
-    };
+'use strict';
+var productArea = document.querySelector('#productArea');
+
+window.onload = function () {
+    fetch("products/",
+        {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (response.status !== 200) {
+
+                return Promise.reject();
+            }
+            return response.json();
+        })
+        .then(products => {
+            products.forEach(function (item, product) {
+                drawProduct(product);
+            });
+        })
+        .catch(() => console.log('Error messages'));
+};
+
+function drawProduct(product) {
+    var productElement = document.createElement('li');
+    var label = document.createElement("label");
+    label.setAttribute("value", product.productName);
+
+    productArea.appendChild(productElement)
+}
